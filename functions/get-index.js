cf902f63502bd29bbe0e1b2e0fc3e44df38d7fd6
Davide Di Somma <davide.disomma@gmail.com>
2020-02-12 22:04:08 +0100

    Setup for debugging

--- functions/get-index.js
+++ functions/get-index.js
@@ -7,6 +7,7 @@ const Mustache = require('mustache');
 const http = require('superagent-promise')(require('superagent'), Promise);
 const aws4 = require('aws4');
 const URL = require('url');
+const awscred = Promise.promisifyAll(require('awscred'));
 
 const awsRegion = process.env.AWS_REGION;
 const cognitoUserPoolId = process.env.cognito_user_pool_id;
@@ -33,6 +34,14 @@ function* getRestaurants() {
     host: url.hostname,
     path: url.pathname
   }
+if (!process.env.AWS_ACCESS_KEY_ID) {
+      //the AWS 4 library that we used to sign our HTTP request doesn't work with AWS profiles. 
+    //So, unfortunately, we have to install another dependency, awscred, and use it to load the credentials from our profile and then set the environment variables manually.
+    let cred = (yield awscred.loadAsync()).credentials;
+
+    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+}
 
   //we'll sign it with aws4.sign, which adds a bunch of headers to the opts object
   aws4.sign(opts);
