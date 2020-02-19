7717141620e760555f98b1aee908cfdb2b898abc
Davide Di Somma <davide.disomma@gmail.com>
2020-02-19 23:26:11 +0100

    Rollback to have acceptance tests green

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -2,6 +2,7 @@
 
 const co = require('co');
 const Promise = require('bluebird');
+const awscred = Promise.promisifyAll(require('awscred'));
 
 let initialized = false;
 
@@ -17,6 +18,15 @@ let init = co.wrap(function* () {
     process.env.cognito_user_pool_id = "us-east-1_SsqleMcdd";
     process.env.cognito_server_client_id = "3im4jk8rd0ct5lv45gpkv1bpgd";
 
+    if (!process.env.AWS_ACCESS_KEY_ID) {
+        //the AWS 4 library that we used to sign our HTTP request doesn't work with AWS profiles. 
+      //So, unfortunately, we have to install another dependency, awscred, and use it to load the credentials from our profile and then set the environment variables manually.
+      let cred = (yield awscred.loadAsync()).credentials;
+  
+      process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+      process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+    }
+  
     console.log("AWS credentials loaded");
     console.log("AWS access key id " + process.env.AWS_ACCESS_KEY_ID);
     console.log("AWS secret access key " + process.env.AWS_SECRET_ACCESS_KEY);
